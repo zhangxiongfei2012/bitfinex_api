7baace1af191796a8f717640d715756f5b41fd37
zhangxiongfei <zhangxiongfei@fqquant.com>
2019-08-03 15:45:36 +0800

    nothing

--- example/collectData.py
+++ example/collectData.py
@@ -23,39 +23,47 @@ def fetch_data(start=1364767200000, stop=1545346740000, symbol='btcusd', interva
         data.extend(res)
         print('Retrieving data from {} to {} for {}'.format(pd.to_datetime(start, unit='ms'),
                                                             pd.to_datetime(end, unit='ms'), symbol))
+        print(len(data))
         time.sleep(1.5)
     return data
 
-
-def calibrate_data(start,path):
-    df = pd.read_csv(path)
+buffer=[]
+def calibrate_data(start,stop,path_new,path_original):
+    df = pd.read_csv(path_original)
     times=df["time"]
     df.set_index('time', inplace=True)
     df.sort_index(inplace=True)
     step=timedelta(minutes=5)
     time_format="%Y-%m-%d %H:%M:%S"
     names = ['time', 'open', 'close', 'high', 'low', 'volume']
-    for t in times.values:
+    while start <= stop:
         current=start.strftime(time_format)
         if current not in df.index:
-            print("current is {}, t is {}".format(current,t))
+            print("current is {}".format(current))
             prev=(start-timedelta(minutes=5)).strftime(time_format)
             new_row=df.loc[prev:prev].values.tolist()
+            while len(new_row) == 0:
+                prev=(datetime.datetime.strptime(prev,time_format)-timedelta(minutes=5)).strftime(time_format)
+                new_row=df.loc[prev:prev].values.tolist()
+                print("prev is {}, new_row is {}".format(prev, new_row))
             new_row[0].insert( 0, current)
-            dfa=pd.DataFrame(new_row,columns=names)
-            dfa.set_index('time', inplace=True)
-            frames = [df, dfa]
-            df=pd.concat(frames)
+            buffer.extend(new_row)
         start=start+step  
+    dfa=pd.DataFrame(buffer,columns=names)
+    dfa.set_index('time', inplace=True)
+    print("The length of missing data is {}".format(len(dfa.index)))
+    frames = [df, dfa]
+    df=pd.concat(frames)
     df.sort_index(inplace=True)
-    df.to_csv(path)
+    df.to_csv(path_new)
 # Define query parameters
 bin_size = '5m'
 limit = 5000
 time_step = 1000*5*60*limit
 start=datetime.datetime(2016, 1, 1, 0, 0,0)
+stop=datetime.datetime(2019, 7, 1, 0, 0,0)
 t_start=unix_time_millis(start)
-t_stop=unix_time_millis( datetime.datetime(2016, 2, 1, 0, 0,0))
+t_stop=unix_time_millis(stop)
 #api_v1 = bitfinex.bitfinex_v1.api_v1()
 #pairs = api_v1.symbols()
 pairs=['btcusd']
@@ -68,9 +76,8 @@ for pair in pairs:
     pair_data = fetch_data(start=t_start, stop=t_stop, symbol=pair, interval=bin_size, tick_limit=limit, step=time_step)
 
     # Remove error messages
-    #ind = [np.ndim(x) != 0 for x in pair_data]
-    #pair_data = [i for (i, v) in zip(pair_data, ind) if v]
-    #pair_data=[pair_data]
+    ind = [np.ndim(x) != 0 for x in pair_data]
+    pair_data = [i for (i, v) in zip(pair_data, ind) if v]
 
     # Create pandas data frame and clean data
     names = ['time', 'open', 'close', 'high', 'low', 'volume']
@@ -81,9 +88,10 @@ for pair in pairs:
     df.sort_index(inplace=True)
 
     print('Done downloading data. Saving to .csv.')
-    path='{}/bitfinex_{}_{}.csv'.format(save_path, pair,bin_size)
-    df.to_csv(path)
+    path_original='{}/bitfinex_{}_{}_original.csv'.format(save_path, pair,bin_size)
+    path_new='{}/bitfinex_{}_{}_new.csv'.format(save_path, pair,bin_size)
+    df.to_csv(path_original)
     print('calibrating data')
-    calibrate_data(start,path)
+    calibrate_data(start,stop,path_new,path_original)
 
 print('Done retrieving data')
